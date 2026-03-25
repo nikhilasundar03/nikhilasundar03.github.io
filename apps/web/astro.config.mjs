391d92b61c0ca0d4154db312ac98059aa39cf019
Nikhila Sundar <nikhilasundar03@gmail.com>
2026-03-25 17:30:11 -0400

    removed profile pic

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -5,8 +5,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nikhilasundar.github.io',
-  base: '/nikhilasundar.github.io',
+  site: 'https://nikhilasundar03.github.io',
+  base: '/',
   output: 'static',
   integrations: [react(), tailwind(), sitemap()],
 });
